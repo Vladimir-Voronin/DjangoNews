b33509fd77645bc12350f6fd6f27c7d6af7445f9
unknown <vladimirvoron7@gmail.com>
2020-02-02 01:51:20 +0300

    Admin panel

--- News/mynews/admin.py
+++ News/mynews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import News
 # Register your models here.
+
+# admin.site.register(News)
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'time_publish', 'status']
+    list_filter = ['status', 'time_create', 'time_publish', 'author']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'time_publish'
+    ordering = ('-status', '-time_publish')
--- News/mynews/models.py
+++ News/mynews/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class NewsManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class News(models.Model):
     SUBJECTS = (
         ('economics', 'Economics'),
@@ -15,18 +20,20 @@ class News(models.Model):
     )
 
     title = models.CharField(max_length=150, db_index=True)
-    slug = models.SlugField(max_length=200, unique_for_date=True)
+    slug = models.SlugField(max_length=200, unique_for_date='time_publish')
     body = models.TextField(max_length=5000, db_index=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news', default=None)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news')
     time_create = models.DateTimeField(auto_now_add=True)
-    time_published = models.DateTimeField(default=timezone.now)
+    time_publish = models.DateTimeField(default=timezone.now)
     time_update = models.DateTimeField(auto_now=True)
     subject_name = models.CharField(max_length=20, choices=SUBJECTS, default='economics')
-    tags = models.ManyToManyField('Tag', related_name='news')
+    tags = models.ManyToManyField('Tag', related_name='news', blank=True)
     status = models.CharField(max_length=10, choices=CHOICES, default='draft')
+    objects = models.Manager()
+    published = NewsManager()
 
     class Meta:
-        ordering = ['-time_published']
+        ordering = ['-time_publish']
 
     def __str__(self):
         return self.title
